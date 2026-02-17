7e6cafd93d5e0ae61a1b87e78280d0b43d3e8539
ezhil-003 <srezhilsivaraj@gmail.com>
2026-02-17 14:02:52 +0530

    Initial Commit

---
+++ src/server/better-auth/server.ts
@@ -0,0 +1,5 @@
+import { auth } from ".";
+import { headers } from "next/headers";
+import { cache } from "react";
+
+export const getSession = cache(async () => auth.api.getSession({ headers: await headers() }));
